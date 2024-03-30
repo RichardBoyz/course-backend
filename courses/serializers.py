fd34604c827e868307ddbb85f8a6ccaae4e67e0b
RichardBoyz <r1382333@gmail.com>
2024-03-30 22:51:00 +0800

    Set courses serializers

--- courses/serializers.py
+++ courses/serializers.py
@@ -6,3 +6,15 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+    def to_internal_value(self, data):
+        user = self.context["request"].user
+
+        data["lecturers"] = [user.id]
+        data["creator"] = user.id
+        return super().to_internal_value(data)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["time_week"] = instance.get_time_week_display()
+        return data
